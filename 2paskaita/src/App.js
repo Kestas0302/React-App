007e8cbf6d2f73ac971e4f9e124e13e8351242c2
Kestas0302 <kestutis.margelis1992@gmail.com>
2022-09-22 21:42:58 +0300

    example with components

--- 2paskaita/src/App.js
+++ 2paskaita/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
-  return <div class="container">Kestutis</div>;
+  return (
+    <div className="container">
+      Kestutis
+      <div className="buttons">
+        <Button text="Say hello"/>
+        <Button text="Click me" type="submit"/>
+        <Button text="Delete" type="button" bgColor="blue"/>
+      </div>
+      <Button text="Take" type="button" bgColor="green"/>
+
+    </div>
+  );
 }
 
 export default App;
---
+++ 2paskaita/src/components/Button/Button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Button.css";
+
+const Button = ({text, type, bgColor}) => ( <div className="button-wrapper"><button type={type} className="button" style={{backgroundColor: bgColor}}>{text}</button>{" "}</div>);
+
+export default Button;
